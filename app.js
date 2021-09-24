b96d690b7d4b0b188042fff3db6c2cb0c4d4ad3c
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2021-09-24 13:32:39 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hello! Script is working.");
+
+alert("This script works!");
+
+var username = prompt("Enter username:");
+var outputMessage = "go away " + username;
+
+alert(outputMessage);
