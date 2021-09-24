dddd2833cec1f001d6eab3287b76f92d7e3280b1
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2021-09-25 00:13:26 +0530

    input output processing done

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var btnTranslate = document.querySelector("#btn-translate");
 // Reference to textarea created in HTML
 var textInput = document.querySelector("#txt-input");
 
-console.log(textInput);
+var textDiv = document.querySelector("#txt-div");
 
-// Listening to "click" event (i.e. detecting click on the button), and executing a function when event occurs (callback)
+
+// Listening to "click" event (i.e. detecting click on the button), and executing a function when event occurs (callback). Here, clickEventHandler is the callback function as it is given to addEventListener as an input/argument.
 function clickEventHandler() {
-    console.log("clicked!");
-    console.log("input", textInput.value);
+    // console.log("clicked!");
+    // console.log("input", textInput.value);
+    textDiv.innerText = "Translated to " + textInput.value;
 }
 
-btnTranslate.addEventListener("click", clickEventHandler)
+btnTranslate.addEventListener("click", clickEventHandler);
 
