8bc7efe829354e4472d451788f3ae186a697cb75
parvez-rashid <84724590+parvez-rashid@users.noreply.github.com>
2021-09-27 05:06:45 +0530

    add style

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ var btnTranslate = document.querySelector("#btn-translate");
 // Reference to textarea created in HTML
 var textInput = document.querySelector("#txt-input");
 
-var textDiv = document.querySelector("#txt-div");
+// Reference to output textarea created in HTML
+var outputDiv = document.querySelector("#output-div");
+
+// API URL
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("An error occurred", error);
+    alert("Server down, please try again later");
+}
 
 
 // Listening to "click" event (i.e. detecting click on the button), and executing a function when event occurs (callback). Here, clickEventHandler is the callback function as it is given to addEventListener as an input/argument.
 function clickEventHandler() {
-    // console.log("clicked!");
-    // console.log("input", textInput.value);
-    textDiv.innerText = "Translated to " + textInput.value;
-}
+
+    var inputText = textInput.value; // Taking input
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
